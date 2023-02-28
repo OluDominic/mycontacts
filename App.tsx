1348796b0cd365544e3a6428f5f4c6a5ada59f08
OluDominic <dominicoludare@gmail.com>
2023-02-28 18:55:18 +0100

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,20 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {SafeAreaView, StatusBar, Text} from 'react-native';
+
+function App(): JSX.Element {
+  return (
+    <SafeAreaView>
+      <StatusBar barStyle="dark-content" />
+      <Text>Hello World</Text>
+    </SafeAreaView>
+  );
+}
+
+export default App;
